67d339e327532a398ddfa4d82a05dc186edcdab3
Ando <iamando@tuta.io>
2024-05-13 17:46:30 +0300

    feat: add code of conduct and contributing md and update docs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Drowser Hub
+
+Tools to help you automate your testing, track progress, and ensure your software is ready for deployment. Say goodbye to manual testing and hello to efficiency 🖥️ 📈
+
+## LICENSE
+
+[MIT](LICENSE).
--- components/pages/home/hero-component.tsx
+++ components/pages/home/hero-component.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { ImportIcon, ArrowRight } from "lucide-react";
+import { ImportIcon } from "lucide-react";
 
 export function HeroComponent() {
   return (
@@ -21,12 +21,6 @@ export function HeroComponent() {
                   href="#"
                 >
                   Import JSON <ImportIcon className="ml-2" />
-                </Link>
-                <Link
-                  className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900/80 backdrop-blur-md px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50/80 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                  href="#"
-                >
-                  Login <ArrowRight className="ml-2" />
                 </Link>
               </div>
             </div>
