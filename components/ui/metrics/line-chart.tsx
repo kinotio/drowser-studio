75b641ace34743a5f2993dfd666efaaffcaa7e2c
Ando <iamando@tuta.io>
2024-05-18 17:37:59 +0300

    feat: separate metrics chart component

---
+++ components/ui/metrics/line-chart.tsx
@@ -0,0 +1,69 @@
+import { ResponsiveLine } from '@nivo/line'
+
+export default function LineChart(props: any) {
+  return (
+    <div {...props}>
+      <ResponsiveLine
+        data={[
+          {
+            id: 'Desktop',
+            data: [
+              { x: 'Jan', y: 43 },
+              { x: 'Feb', y: 137 },
+              { x: 'Mar', y: 61 },
+              { x: 'Apr', y: 145 },
+              { x: 'May', y: 26 },
+              { x: 'Jun', y: 154 },
+              { x: 'Jul', y: 111 },
+              { x: 'Aug', y: 157 },
+              { x: 'Sep', y: 129 },
+              { x: 'Oct', y: 150 },
+              { x: 'Nov', y: 119 },
+              { x: 'Dec', y: 72 }
+            ]
+          }
+        ]}
+        margin={{ top: 10, right: 10, bottom: 40, left: 40 }}
+        xScale={{
+          type: 'point'
+        }}
+        yScale={{
+          type: 'linear'
+        }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 0,
+          tickPadding: 16
+        }}
+        axisLeft={{
+          tickSize: 0,
+          tickValues: 5,
+          tickPadding: 16
+        }}
+        colors={['#4669B3']}
+        pointSize={6}
+        useMesh={true}
+        gridYValues={6}
+        theme={{
+          tooltip: {
+            chip: {
+              borderRadius: '9999px'
+            },
+            container: {
+              fontSize: '12px',
+              textTransform: 'capitalize',
+              borderRadius: '6px'
+            }
+          },
+          grid: {
+            line: {
+              stroke: '#f3f4f6'
+            }
+          }
+        }}
+        role='application'
+      />
+    </div>
+  )
+}
