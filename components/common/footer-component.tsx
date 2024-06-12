712b1e7ac00c3b37b9de3575cb23d45092ae9638
Ando <andostronaut@gmail.com>
2024-06-12 09:26:15 +0300

    feat: update landing page ui

--- components/common/footer-component.tsx
+++ components/common/footer-component.tsx
@@ -3,13 +3,16 @@ import Image from 'next/image'
 
 export function FooterComponent() {
   return (
-    <footer className='w-full bg-gray-100/40 py-4'>
-      <div className='flex items-center justify-end pl-4 gap-2 px-6'>
+    <footer className='flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t'>
+      <p className='text-xs text-gray-500 dark:text-gray-400'>
+        © 2024 Drowser Studio. All rights reserved.
+      </p>
+      <nav className='sm:ml-auto flex gap-2 items-center'>
         <span className='text-sm font-semibold'>By</span>
         <Link href={'https://github.com/kinotio'}>
           <Image src='/images/kinotio-logo-dark.png' alt='Kinotio Logo' width={90} height={90} />
         </Link>
-      </div>
+      </nav>
     </footer>
   )
 }
