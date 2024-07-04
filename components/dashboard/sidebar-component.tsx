6f05bf665c9d936122b8fb2584ddae33bf5ae684
Ando <andostronaut@gmail.com>
2024-07-04 11:19:13 +0300

    feat: add space from the left for each link case on sidebar

--- components/dashboard/sidebar-component.tsx
+++ components/dashboard/sidebar-component.tsx
@@ -104,7 +104,7 @@ export default function SidebarComponent() {
                           {c.browser === browser ? (
                             <Link
                               key={c.id}
-                              className={`flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ${pathName.startsWith(PATH.DASHBOARD_CASES) && params.id === c.id ? 'text-gray-900' : ''}`}
+                              className={`ml-6 flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ${pathName.startsWith(PATH.DASHBOARD_CASES) && params.id === c.id ? 'text-gray-900' : ''}`}
                               href={`/dashboard/cases/${c.id}`}
                             >
                               <ClipboardIcon className='h-4 w-4' />
