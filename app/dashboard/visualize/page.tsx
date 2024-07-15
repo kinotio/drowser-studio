11513f2114786460fe94ae004d8dabf30d3e4dc7
Ando <65276708+andostronaut@users.noreply.github.com>
2024-07-15 21:25:56 +0300

    feat: customize node ui in graph and use data to show some info for each case

--- app/dashboard/visualize/page.tsx
+++ app/dashboard/visualize/page.tsx
@@ -1,21 +1,40 @@
 'use client'
 
 import { useState, useEffect, useRef, MutableRefObject } from 'react'
-import { Canvas, CanvasPosition, CanvasRef } from 'reaflow'
-import { ZoomInIcon, ZoomOutIcon, MaximizeIcon, FolderRootIcon } from 'lucide-react'
+import { Canvas, CanvasPosition, CanvasRef, Node as ReaflowNode } from 'reaflow'
+import {
+  ZoomInIcon,
+  ZoomOutIcon,
+  MaximizeIcon,
+  FolderRootIcon,
+  GlobeIcon,
+  TimerIcon,
+  CalendarIcon,
+  ClipboardIcon,
+  CircleCheckIcon,
+  CircleXIcon,
+  BoxesIcon
+} from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip'
+import { Card, CardContent, CardFooter } from '@/components/ui/card'
+import { Badge } from '@/components/ui/badge'
 
 import { useStore } from '@/hooks/use-store'
 import { useReportStore } from '@/hooks/use-report-store'
 
-import { TDrowserReport } from '@/lib/definitions'
+import { TDrowserReport, TContentCase, TContentSubCase } from '@/lib/definitions'
+import { humanizeDuration, readableTimestamp } from '@/lib/utils'
+import { CASE_STATUS } from '@/lib/constants'
 
 type Node = {
   id: string
-  text: string
+  text?: string
   nodes?: Node[]
+  width?: number
+  height?: number
+  data?: TContentCase | TContentSubCase | any
 }
 
 type Edge = {
@@ -39,19 +58,24 @@ const Page = () => {
 
   const mainNode: Node = {
     id: 'main',
-    text: 'Main Node',
+    text: 'Cases',
+    data: {
+      name: 'Main'
+    },
     nodes: content?.drowser.cases.map((group) => ({
       id: group.id,
       text: group.id,
+      data: { ...group },
       nodes: group.cases.map((testCase) => ({
         id: testCase.id,
-        text: testCase.id
+        text: testCase.id,
+        data: { ...testCase }
       }))
     }))
   }
 
   const flattenNodes = (node: Node, flatNodes: Node[] = []): Node[] => {
-    flatNodes.push({ id: node.id, text: node.text })
+    flatNodes.push({ id: node.id, width: 500, height: 125, data: node.data })
     if (node.nodes) {
       node.nodes.forEach((child) => flattenNodes(child, flatNodes))
     }
@@ -78,11 +102,12 @@ const Page = () => {
   return (
     <div className='w-[80vw] h-full relative overflow-hidden'>
       <Canvas
+        arrow={null}
         panType='drag'
         maxZoom={10}
         minZoom={-0.9}
-        maxWidth={3000}
-        maxHeight={1500}
+        maxWidth={10000}
+        maxHeight={10000}
         zoom={zoom}
         ref={ref}
         defaultPosition={CanvasPosition.LEFT}
@@ -94,6 +119,86 @@ const Page = () => {
         edges={edges}
         className='p-0 m-0 border'
         onZoomChange={(z) => setZoom(z)}
+        node={
+          <ReaflowNode>
+            {(event) => (
+              <foreignObject width={event.width} height={event.height} x={0} y={0}>
+                {event.node.data && Object.keys(event.node?.data).length !== 0 ? (
+                  <Card className='h-full w-full flex items-center justify-center flex-col bg-white border-none rounded-none'>
+                    <CardContent className='w-full h-full py-6'>
+                      <div className='flex  items-center justify-center gap-4'>
+                        <div className='flex gap-2 items-center'>
+                          <div className='bg-gray-200 dark:bg-gray-800 rounded-full w-8 h-8 flex items-center justify-center'>
+                            {event.node.data.name ? (
+                              <div>
+                                {event.node.data.name === 'Main' ? (
+                                  <FolderRootIcon className='h-4 w-4 text-gray-500 dark:text-gray-400' />
+                                ) : (
+                                  <ClipboardIcon className='h-4 w-4 text-gray-500 dark:text-gray-400' />
+                                )}
+                              </div>
+                            ) : (
+                              <BoxesIcon className='h-4 w-4 text-gray-500 dark:text-gray-400' />
+                            )}
+                          </div>
+                          <div>
+                            <h4 className='font-medium'>
+                              {!event.node.data.name ? 'Group' : event.node.data.name}
+                            </h4>
+                          </div>
+                        </div>
+
+                        {event.node.data.status ? (
+                          <div className='flex gap-4'>
+                            <p
+                              className={`capitalize text-sm text-gray-500 dark:text-gray-400 flex gap-2 items-center ${
+                                event.node.data.status === CASE_STATUS.passed
+                                  ? 'text-green-500'
+                                  : 'text-red-500'
+                              }`}
+                            >
+                              {event.node.data.status === CASE_STATUS.passed ? (
+                                <CircleCheckIcon size='16' />
+                              ) : (
+                                <CircleXIcon size='16' />
+                              )}
+                              {event.node.data.status}
+                            </p>
+                          </div>
+                        ) : null}
+                      </div>
+                    </CardContent>
+
+                    <CardFooter>
+                      <div className='flex text-sm text-gray-500 dark:text-gray-400 gap-4 justify-center w-full'>
+                        {event.node.data.browser ? (
+                          <div>
+                            <Badge variant='secondary'>
+                              <TimerIcon size='16' className='mr-1' />
+                              {humanizeDuration(
+                                event.node.data.duration ?? event.node.data.avg_duration
+                              )}
+                            </Badge>
+                            <Badge>
+                              <CalendarIcon size='16' className='mr-1' />
+                              {readableTimestamp(event.node.data.timestamp ?? event.node.data.time)}
+                            </Badge>
+                            <Badge variant='outline'>
+                              <GlobeIcon size='16' className='mr-1' />
+                              {event.node.data.browser}
+                            </Badge>
+                          </div>
+                        ) : (
+                          <Badge>This is the main root of the graph.</Badge>
+                        )}
+                      </div>
+                    </CardFooter>
+                  </Card>
+                ) : null}
+              </foreignObject>
+            )}
+          </ReaflowNode>
+        }
       />
 
       <ZoomControls passedRef={ref} />
