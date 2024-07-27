27e8008176f2b366d324fe3be219e4ad1344b9fd
Ando <65276708+andostronaut@users.noreply.github.com>
2024-07-27 11:27:23 +0300

    feat: update kinotio logo and remove unused file on public folder

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 
-// import HeaderComponent from '@/components/dashboard/header-component'
+// import { Header } from '@/components/dashboard/header'
 import { Sidebar } from '@/components/dashboard/sidebar'
 import { Footer } from '@/components/dashboard/footer'
 import { Toaster } from '@/components/ui/sonner'
@@ -18,10 +18,10 @@ const Layout = ({
     <div className='flex h-screen'>
       <Sidebar />
       <div className='flex flex-col w-full'>
-        {/* <HeaderComponent /> */}
-        <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 overflow-auto'>
+        {/* <Header /> */}
+        <div className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 overflow-auto'>
           {children}
-        </main>
+        </div>
         <Footer />
         <Toaster position='bottom-left' />
       </div>
