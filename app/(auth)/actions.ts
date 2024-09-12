7735e57e2c28915b5bd38ce0dc6aff9dcb306cdf
Ando <65276708+andostronaut@users.noreply.github.com>
2024-09-12 21:55:38 +0300

    feat: handle user account register

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -1,5 +1,35 @@
 'use server'
 
-export const Login = () => {}
+import { redirect } from 'next/navigation'
+import { supabase } from '@/lib/supabase/server'
 
-export const Register = () => {}
+import { LoginFormData, RegisterFormData } from '@/app/(auth)/auth/page'
+import { revalidatePath } from 'next/cache'
+
+export const login = async () => {}
+
+export const register = async (form: RegisterFormData) => {
+  const { data } = await supabase.from('users').select('*').eq('username', form.username)
+
+  if (Array.isArray(data) && data.length > 0) {
+    return { error: 'An error occurred while signup, username already taken' }
+  }
+
+  const { error } = await supabase.auth.signUp({
+    email: form.email,
+    password: form.password,
+    options: {
+      data: {
+        name: form.name,
+        username: form.username
+      }
+    }
+  })
+
+  if (error) {
+    return { error: `An error occurred while signup: ${error.message}` }
+  }
+
+  revalidatePath('/auth', 'layout')
+  redirect('/auth')
+}
