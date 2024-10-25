43759095fe8e174662bce7cbbae96e94b395a223
Ando <hi@andostronaut.com>
2024-10-25 23:03:01 +0300

    chore: clean logto config

--- src/config/logto.ts
+++
@@ -1,8 +0,0 @@
-export const logtoConfig = {
-  endpoint: 'https://logto.kinotio.io/',
-  appId: 'kxu2x62bowdoot5wiqh39',
-  appSecret: 'AG8YcbVFp1bWkSyBLwCj9NSG7h27dkAv',
-  baseUrl: 'http://localhost:3000', // Change to your own base URL
-  cookieSecret: 'EerD5ygdBVq7FTl7EYQQgXLCendAtamc', // Auto-generated 32 digit secret
-  cookieSecure: process.env.NODE_ENV === 'production'
-}
