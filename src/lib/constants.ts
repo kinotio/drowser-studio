993d5c08f766a28dfdce7c86c1323515335a6657
Ando <hi@andostronaut.com>
2024-10-28 15:15:07 +0300

    feat: impl and filter activities by search

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,27 @@
 import pkg from '../../package.json'
 
 export const APP_VERSION = `v${pkg.version}`
+
+const BASE_EVENT_TYPES = [
+  'unknown',
+  'login',
+  'account_created',
+  'password_change',
+  'report_added',
+  'inboxes_preferences_change',
+  'profile_information_change',
+  'logout'
+] as string[]
+
+export const ACTIVITIES_TYPES = BASE_EVENT_TYPES
+
+export const EVENT_TYPES: { [key: string]: string } = {
+  unknown: 'unknown',
+  login: 'login',
+  accountCreated: 'account_created',
+  passwordChange: 'password_change',
+  reportAdded: 'report_added',
+  inboxesPreferencesChange: 'inboxes_preferences_change',
+  profileInformationChange: 'profile_information_change',
+  logout: 'logout'
+}
