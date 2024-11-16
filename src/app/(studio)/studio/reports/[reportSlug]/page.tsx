6286ca1066cd0b6e6b89a4d71aa05a301601b303
Ando <hi@andostronaut.com>
2024-11-16 15:59:42 +0300

    fix: make report more reponsive on metrics

--- src/app/(studio)/studio/reports/[reportSlug]/page.tsx
+++ src/app/(studio)/studio/reports/[reportSlug]/page.tsx
@@ -32,7 +32,7 @@ const Page = ({ params }: { params: { reportSlug: string } }) => {
   return (
     <div>
       {!isEmpty(metrics) ? (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-6'>
           <Card>
             <CardHeader>
               <CardTitle>Total Tests</CardTitle>
