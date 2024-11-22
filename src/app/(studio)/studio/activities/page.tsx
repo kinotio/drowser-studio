75794e86d5aa2aaa6e76f918d3cf839e65720ed3
Ando <hi@andostronaut.com>
2024-11-22 15:51:32 +0300

    feat: sort activities by created

--- src/app/(studio)/studio/activities/page.tsx
+++ src/app/(studio)/studio/activities/page.tsx
@@ -87,7 +87,8 @@ const Page = () => {
     pocketbase
       .collection('activities')
       .getList(currentPage, itemsPerPage, {
-        filter: `user_id = "${userId}" && type ~ "${searchTerm}" && description ~ "${searchTerm}"`
+        filter: `user_id = "${userId}" && type ~ "${searchTerm}" && description ~ "${searchTerm}"`,
+        sort: '-created'
       })
       .then((data) => {
         setActivities(data.items as Activity[])
