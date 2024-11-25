68e92caa01d3ce947c3fdcc67bf150238ea58c50
Ando <hi@andostronaut.com>
2024-11-25 12:05:30 +0300

    fix: hide drowser ai menu for now

--- src/app/api/clerk/webhooks/route.ts
+++ src/app/api/clerk/webhooks/route.ts
@@ -1,3 +1,4 @@
+
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
--- src/components/report-sidebar.tsx
+++ src/components/report-sidebar.tsx
@@ -119,7 +119,7 @@ export const ReportSidebar = () => {
           </Link>
         </div>
 
-        <div className='flex'>
+        <div className='hidden'>
           <Link
             className='pointer-events-none flex items-center gap-3 rounded-lg px-2 py-2 transition-all'
             href={`${PATH.STUDIO_REPORTS}/${paramsReportSlug}/ai`}
