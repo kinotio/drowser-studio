040b3b9327347c1e32cf5b1b251af35c607d7cc1
Ando <hi@andostronaut.com>
2024-12-05 18:47:18 +0300

    fix: toogle theme button width

--- src/components/toogle-theme.tsx
+++ src/components/toogle-theme.tsx
@@ -8,14 +8,14 @@ export const ToggleTheme = ({ inStudio = false }: { inStudio?: boolean }) => {
     return (
       <span
         onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        className='justify-start cursor-pointer'
+        className='justify-start cursor-pointer w-full'
       >
-        <div className='flex gap-2 dark:hidden items-center'>
+        <div className='flex gap-2 dark:hidden items-center w-full'>
           <Moon size={20} />
           <span>Dark</span>
         </div>
 
-        <div className='dark:flex gap-2 hidden items-center'>
+        <div className='dark:flex gap-2 hidden items-center w-full'>
           <Sun size={20} />
           <span>Light</span>
         </div>
