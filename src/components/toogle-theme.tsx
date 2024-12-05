5f32cf1b2e5dd0e2d056be0ee6b895abec23d95e
Ando <hi@andostronaut.com>
2024-12-05 15:44:05 +0300

    feat: update ui

--- src/components/toogle-theme.tsx
+++ src/components/toogle-theme.tsx
@@ -2,8 +2,28 @@ import { useTheme } from 'next-themes'
 import { Moon, Sun } from 'lucide-react'
 import Link from 'next/link'
 
-export const ToggleTheme = () => {
+export const ToggleTheme = ({ inStudio = false }: { inStudio: boolean }) => {
   const { theme, setTheme } = useTheme()
+
+  if (inStudio) {
+    return (
+      <Link
+        href='#'
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        className='justify-start'
+      >
+        <div className='flex gap-2 dark:hidden items-center'>
+          <Moon size={20} />
+          <span>Dark</span>
+        </div>
+
+        <div className='dark:flex gap-2 hidden items-center'>
+          <Sun size={20} />
+          <span>Light</span>
+        </div>
+      </Link>
+    )
+  }
 
   return (
     <Link
@@ -11,15 +31,17 @@ export const ToggleTheme = () => {
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       className='justify-start'
     >
-      <div className='flex gap-2 dark:hidden items-center'>
-        <Moon size={20} className='mr-2' />
-        <span>Dark</span>
+      <div className='flex gap-2 dark:hidden'>
+        <Moon size={20} />
+        <span className='block lg:hidden'>Dark</span>
       </div>
 
-      <div className='dark:flex gap-2 hidden items-center'>
-        <Sun size={20} className='mr-2' />
-        <span>Light</span>
+      <div className='dark:flex gap-2 hidden'>
+        <Sun size={20} />
+        <span className='block lg:hidden'>Light</span>
       </div>
+
+      <span className='sr-only'>Toggle theme</span>
     </Link>
   )
 }
