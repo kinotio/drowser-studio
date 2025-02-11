424d53d29c5b305fc045e482c473b5728e5acb80
Ando <aramanamihanta@gmail.com>
2025-02-11 17:30:08 +0300

    fix: remove save report action

--- src/server/actions/report.ts
+++ src/server/actions/report.ts
@@ -3,8 +3,6 @@
 import { drizzle, eq, desc } from '@/server/drizzle'
 import { reports } from '@/server/db/schema'
 
-import { Report } from '@/lib/definitions'
-
 export const getAllReport = async (payload: { userId: string }) => {
   return drizzle.select().from(reports).where(eq(reports.user_id, payload.userId))
 }
@@ -17,8 +15,4 @@ export const getLastThreeReport = async (payload: { userId: string }) => {
     .orderBy(desc(reports.created))
     .limit(3)
     .offset(0)
-}
-
-export const saveReport = async (payload: Report) => {
-  return drizzle.insert(reports).values(payload)
 }
