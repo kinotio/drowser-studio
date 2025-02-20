c6184efe11b3bfaea9f172ca8f45f3683fb175ad
Ando <hi@andostronaut.com>
2025-02-20 21:53:36 +0300

    feat: migrate all activity logic to server action (#448)
    * feat: migrate all activity logic to server action
    
    * feat: use server action activity while fetching latest activity on overview

--- src/server/actions/activity.ts
+++ src/server/actions/activity.ts
@@ -1,8 +1,28 @@
 'use server'
 
-import { drizzle, eq, desc } from '@/server/drizzle'
+import { drizzle, eq, desc, and, sql, count } from '@/server/drizzle'
 import { activities } from '@/server/db/schema'
-import { Activity } from '@/server/types'
+import { ActivityInferType } from '@/server/types'
+
+export const getAllActivities = async (payload: {
+  userId: string
+  currentPage: number
+  itemsPerPage: number
+  searchTerm: string
+}) => {
+  return drizzle
+    .select()
+    .from(activities)
+    .where(
+      and(
+        eq(activities.user_id, payload.userId),
+        sql`description LIKE ${`%${payload.searchTerm}%`}`
+      )
+    )
+    .orderBy(desc(activities.created))
+    .limit(payload.itemsPerPage)
+    .offset((payload.currentPage - 1) * payload.itemsPerPage)
+}
 
 export const getLastThreeActivity = async (payload: { userId: string }) => {
   return drizzle
@@ -14,11 +34,21 @@ export const getLastThreeActivity = async (payload: { userId: string }) => {
     .offset(0)
 }
 
-export const saveActivity = async (payload: Activity) => {
-  return drizzle.insert(activities).values({
-    user_id: payload.userId,
-    type: payload.type,
-    description: payload.description,
-    device: payload.device
-  })
+export const saveActivity = async (payload: ActivityInferType) => {
+  return drizzle
+    .insert(activities)
+    .values({
+      user_id: payload.userId,
+      type: payload.type,
+      description: payload.description,
+      device: payload.device
+    })
+    .returning()
+}
+
+export const countActivities = async (payload: { userId: string }) => {
+  return drizzle
+    .select({ count: count(activities.id) })
+    .from(activities)
+    .where(eq(activities.user_id, payload.userId))
 }
