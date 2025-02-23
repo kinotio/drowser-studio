0874e1bd431f15c053cbcdf0feb26e598f6a1558
Ando <aramanamihanta@gmail.com>
2025-02-23 16:02:31 +0300

    feat: implement prompt and chat with ai assistant

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    CRYPTO_PASSPHRASE: process.env.CRYPTO_PASSPHRASE
+    CRYPTO_PASSPHRASE: process.env.CRYPTO_PASSPHRASE,
+    AI_BASE_URL: process.env.AI_BASE_URL,
+    AI_TOKEN: process.env.AI_TOKEN
   }
 }
 
--- src/hooks/use-ai.tsx
+++ src/hooks/use-ai.tsx
@@ -1,50 +1,57 @@
 import { useState, useCallback } from 'react'
 
-export const useAI = (type: 'chat' | 'prompt', message: string) => {
+import { Message } from '@/lib/definitions'
+
+export const useAI = (type: 'chat' | 'prompt') => {
   const [responseData, setResponseData] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  const fetchData = useCallback(async () => {
-    if (!message || !type) return
+  const fetchData = useCallback(
+    async (input: string | Message[]) => {
+      if (!input || !type) return
 
-    const baseUrl = process.env.AI_BASE_URL
-    const endpoint = type === 'chat' ? `${baseUrl}/api/ai/chat` : `${baseUrl}/api/ai/generate`
-    const token = process.env.AI_TOKEN
+      const baseUrl = process.env.AI_BASE_URL
+      const endpoint = type === 'chat' ? `${baseUrl}/api/ai/chat` : `${baseUrl}/api/ai/generate`
+      const token = process.env.AI_TOKEN
 
-    setIsLoading(true)
-    try {
-      const response = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify({ message })
-      })
+      const body = type === 'chat' ? { messages: input } : { prompt: input }
 
-      if (!response.body) {
-        throw new Error('ReadableStream not yet supported in this browser.')
+      setIsLoading(true)
+      try {
+        const response = await fetch(endpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(body)
+        })
+
+        if (!response.body) {
+          throw new Error('ReadableStream not yet supported in this browser.')
+        }
+
+        const reader = response.body.getReader()
+        const decoder = new TextDecoder()
+        let done = false
+        let responseText = ''
+
+        while (!done) {
+          const { value, done: doneReading } = await reader.read()
+          done = doneReading
+          const chunkValue = decoder.decode(value, { stream: true })
+          responseText += chunkValue
+        }
+
+        setResponseData(responseText)
+      } catch (error) {
+        console.error('Error fetching stream:', error)
+      } finally {
+        setIsLoading(false)
       }
-
-      const reader = response.body.getReader()
-      const decoder = new TextDecoder()
-      let done = false
-      let responseText = ''
-
-      while (!done) {
-        const { value, done: doneReading } = await reader.read()
-        done = doneReading
-        const chunkValue = decoder.decode(value, { stream: true })
-        responseText += chunkValue
-      }
-
-      setResponseData(responseText)
-    } catch (error) {
-      console.error('Error fetching stream:', error)
-    } finally {
-      setIsLoading(false)
-    }
-  }, [message, type])
+    },
+    [type]
+  )
 
   return { responseData, isLoading, fetchData }
 }
